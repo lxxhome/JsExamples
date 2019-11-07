29b62f464e6d7f18f02f2f66826aa93145d308a5
liuxixi <liuxixi@bestseller.com.cn>
2019-11-07 18:21:22 +0800

    test

--- test/arrayContent.js
+++
@@ -1,7 +0,0 @@
-let brands = ['ONLY', 'JACKJONES', 'VEROMODA', 'SELECTED'];
-console.log(brands.includes('ONLY'));
-if('ONLY'.indexOf(brands)>0 ){
-    console.log('ONLY'.indexOf(brands));
-}else{
-    console.log('no')
-}
--- test/objectVarTest.js
+++
@@ -1,8 +0,0 @@
-let brand='VEROMODA';
-let channel=1;
-let query={phone:'18289098890'};
-
-Object.assign(query,{brand},{channel});
-
-console.log(query);
-console.log(JSON.stringify(query))
