7eedc392f8f87f44895500d04fc2b43215962c81
liuxixi <liuxixi@bestseller.com.cn>
2020-03-18 15:51:31 +0800

    添加：深度判断对角中是否有相同元素

--- test/objectSameElement.js
+++ test/objectSameElement.js
@@ -26,33 +26,40 @@ function equalsObj(oldData, newData) {
     if (isObject(oldData) && isObject(newData)) {
         //      遍历所有对象中所有属性,判断元素是否相同
         for (const key in oldData) {
-            if (oldData.hasOwnProperty(key)) {
-                // console.log(oldData[key]);
-                console.log(isObject(oldData[key]));
-                if (equalsObj(oldData[key], newData[key]))
+            for (const nkey in newData) {
+                if (equalsObj(oldData[key], newData[nkey]))
                 //      对象中有相同属性 返回true 
                 {
                     return true;
-                } else {
-                    equalsObj(oldData, newData);
                 }
             }
+            // if (oldData.hasOwnProperty(key)) {
+            //     // console.log(oldData[key]);
+            //     console.log(isObject(oldData[key]));
+            //     if (equalsObj(oldData[key], newData[key]))
+            //     //      对象中有相同属性 返回true 
+            //     {
+            //         return true;
+            //     } else {
+            //         equalsObj(oldData, newData);
+            //     }
+            // }
         }
-} else if (isArray(oldData) && isArray(oldData)) {
-    //      类型为数组
+    } else if (isArray(oldData) && isArray(oldData)) {
+        //      类型为数组
 
-    for (let i = 0, length = oldData.length; i < length; i++) {
-        if (equalsObj(oldData[i], newData[i]))
-            //      如果数组元素中具有相同元素,返回true
-            return true;
+        for (let i = 0, length = oldData.length; i < length; i++) {
+            if (equalsObj(oldData[i], newData[i]))
+                //      如果数组元素中具有相同元素,返回true
+                return true;
+        }
+    } else {
+        //      其它类型,均返回false
+        return false;
     }
-} else {
-    //      其它类型,均返回false
-    return false;
-}
 
-//      走到这里,说明数组或者对象中所有元素不相同,返回false
-return false;
+    //      走到这里,说明数组或者对象中所有元素不相同,返回false
+    return false;
 };
 
 
@@ -62,13 +69,18 @@ return false;
 
 let oldData = {
     0: {
-        name: 1,
-        sex: 1
+        name: 3,
+        sex: 0
     },
     1: {
-        name: 1,
+        name: 4,
         sex: 0
-    }
+    },
+    2: {
+        name: 5,
+        sex: 0
+    },
+    3: 0
 }
 
 let newData = {
